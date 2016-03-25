88063e75a7e73a93fafc95c4d49199d2320ef1fa
Matthieu Rébillard <hello@matthieurebillard.fr>
2016-03-25 13:30:28 +0100

    Update references

---
+++ references/index.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset='utf-8'>
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1">
+	<meta name="description" content="Matthieu Rébillard est développeur web sur Nantes et sa région. Intégration et développement de sites internet et boutiques en ligne">
+	<meta name="author" content="Matthieu Rébillard">
+	<title>Développeur web Front-end / Back-end en freelance à Nantes | Matthieu Rébillard</title>
+	<link rel="icon" type="image/png" href="img/favicon-128.png">
+
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/fonts.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body ontouchstart="">
+	<div class="wrapper">
+		<div class="trait-vertical court"></div>
+		<div class="portrait"><a href="//matthieurebillard.fr"><img src="img/photo-mat.jpg" alt="Matthieu Rébillard - Développeur web à Nantes"></a></div>
+		<div class="trait-vertical court"></div>
+		<section class="presentation">
+			<!--<h1>Salut, je suis Matthieu Rébillard,<br>développeur et intégrateur web<br><span class="quality-wrapper"><span class="quality"></span></span></h1>-->
+			<h1>Matthieu Rébillard - Références</h1>
+		</section>
+		<div class="trait-vertical"></div>
+		<section class="references">
+			
+			<figure>
+				<a href="//www.pretpro.fr" target="_blank"><img src="img/ref/pretpro-01.jpg" alt="Pret Pro"></a>
+				<a href="//www.pretpro.fr" target="_blank"><img src="img/ref/pretpro-02.jpg" alt="Pret Pro"></a>
+				<a href="//www.pretpro.fr" target="_blank"><img src="img/ref/pretpro-03.jpg" alt="Pret Pro"></a>
+				<figcaption>
+						<h2>Site internet pour pr€t Pro, courtiers en prêts professionnels</h2>
+						<p>Client national, ce site est en cours de développement, la sortie est prévue début janvier.</p>
+						<ul>
+							<li>WordPress</li>
+							<li>Animations CSS3 et Javascript</li>
+							<li>Google Maps</li>
+							<li>Responsive</li>
+							<li>Connexion API sur l'extranet du même client</li>
+						</ul>
+				</figcaption>
+			</figure>
+			<figure>
+				<a href="//extranet.pretpro.fr" target="_blank"><img src="img/ref/pretpro-extranet-06.jpg" alt="Pret Pro, extranet"></a>
+				<figcaption>
+						<h2>Extranet de suivi et gestion d'activité pour pr€t Pro, courtiers en prêts professionnels</h2>
+						<p>Cette interface est connectée en API REST à la source de données Salesforce et permet de mettre à disposition de l'ensemble des acteurs de l'activité (commerciaux, directeurs, banques et clients) les données sur les dossiers de financements.</p>
+						<ul>
+							<li>Codeigniter</li>
+							<li>Responsive</li>
+							<li>API REST avec connexion oAuth2</li>
+							<li>Différents niveaux de connexion</li>
+							<li>Plusieurs mois de développement</li>
+						</ul>
+						<p><small>Note : impossible de vous présenter une version de démonstration de cette interface, les données sont confidentielles</small></p>
+				</figcaption>
+			</figure>
+			<figure>
+				<a href="//orci-pdl.fr" target="_blank"><img src="img/ref/orci-01.jpg" alt="Orci"></a>
+				<figcaption>
+						<h2>Site internet pour l'ORCI, observatoire régional de l'industrie</h2>
+						<p>Client régional</p>
+						<ul>
+							<li>WordPress</li>
+							<li>Animations CSS3 et Javascript</li>
+						</ul>
+				</figcaption>
+			</figure>
+			<figure>
+				<a href="//csc-solutions.eu" target="_blank"><img src="img/ref/csc-01.jpg" alt="CSC Solutions"></a>
+				<figcaption>
+						<h2>Site "One page" pour CSC Solutions, intermédiaire en contrôle qualité de produits pétroliers</h2>
+						<p>Client national</p>
+						<ul>
+							<li>WordPress</li>
+						</ul>
+				</figcaption>
+			</figure>
+			<figure>
+				<a href="//intranet.astikoto.fr" target="_blank"><img src="img/ref/astikoto-01.jpg" alt="Intranet Astikoto"></a>
+				<a href="//intranet.astikoto.fr" target="_blank"><img src="img/ref/astikoto-02.jpg" alt="Intranet Astikoto"></a>
+				<a href="//intranet.astikoto.fr" target="_blank"><img src="img/ref/astikoto-03.jpg" alt="Intranet Astikoto"></a>
+				<figcaption>
+						<h2>Intranet entreprise pour Astikoto, un réseau de franchises de centres de lavage automobile écologiques</h2>
+						<p>Client régional. Cette interface SAAS (Service as a software) permet la gestion globale de l'ensemble de l'activité des centres de lavage auto.</p>
+						<ul>
+							<li>Codeigniter</li>
+							<li>Actions en AJAX</li>
+							<li>Plusieurs mois de développement</li>
+						</ul>
+						<p><small>Note : impossible de vous présenter une version de démonstration de cette interface, les données sont confidentielles</small></p>
+				</figcaption>
+			</figure>
+			<figure>
+				<a href="//balade-des-ateliers.fr" target="_blank"><img src="img/ref/balade-01.jpg" alt="Balade des ateliers"></a>
+				<figcaption>
+						<h2>Site pour la Balade des Ateliers, manifestation artistique annuelle dans les quartiers Chantenay et Ste-Anne. Une trentaine d'ateliers ouvrent leurs portes le temps d'un week-end</h2>
+						<p>Client local</p>
+						<ul>
+							<li>WordPress</li>
+						</ul>
+				</figcaption>
+			</figure>
+			<figure>
+				<a href="//jordinateur.fr" target="_blank"><img src="img/ref/jordinateur-01.jpg" alt="Jordinateur"></a>
+				<figcaption>
+						<h2>Site internet pour la Jordinateur, maintenance informatique</h2>
+						<p>Client local</p>
+						<ul>
+							<li>WordPress</li>
+						</ul>
+				</figcaption>
+			</figure>
+			<figure>
+				<a href="//carte.pretpro.fr" target="_blank"><img src="img/ref/pretpro-carte-05.jpg" alt="Pret Pro, carte"></a>
+				<figcaption>
+						<h2>Géolocalisation de l'ensemble des données de pr€t Pro, courtiers en prêts professionnels</h2>
+						<p>Cette carte géolocalise les milliers de contacts, centaines de dossiers de financements et les centaines de RDVs sur une carte interactive mise à jour dynamiquement.</p>
+						<ul>
+							<li>Google Maps</li>
+						</ul>
+						<p><small>Note : impossible de vous présenter une version de démonstration de cette interface, les données sont confidentielles</small></p>
+				</figcaption>
+			</figure>
+		</section>
+		<div class="trait-vertical"></div>
+		<section class="liens">
+			<h2>Informations sur les réseaux</h2>
+			<ul>
+				<li><a class="twitter" href="https://twitter.com/matrebillard" target="_blank">
+				<i class="icon icon-hyp-twitter"></i>
+					</a></li>
+				<li><a class="linkedin" href="https://fr.linkedin.com/in/matthieurebillard" target="_blank">
+					<i class="icon icon-hyp-linkedin"></i>
+				</a></li>
+				<li><a class="viadeo" href=" http://www.viadeo.com/fr/profile/matthieu.rebillard" target="_blank">
+					<i class="icon icon-hyp-viadeo"></i>
+				</a></li>
+				<li><a class="cv" href="http://matthieurebillard.fr/assets/cv-matthieu-rebillard-dev-web.pdf" target="_blank">
+					<i class="icon icon-hyp-cv"></i>
+				</a></li>
+				<li><a class="mail mail-link" title="Activer Javascript pour voir l'adresse e-mail">
+					<i class="icon icon-hyp-mail"></i>
+				</a></li>
+			</ul>
+
+		</section>
+		<script src="js/jquery-1.11.1.min.js"></script>
+		<script src="js/app.js"></script>
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-55036688-1', 'auto');
+		ga('send', 'pageview');
+	</script>
+</body>
+</html>
