c7b3944b4bf4dcd94294c9f9cbcd94111d579a31
Matthieu <hello@matthieurebillard.fr>
2019-06-13 22:25:54 +0200

    UA vers GTM

--- index.php
+++ index.php
@@ -2,6 +2,14 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-PBBH6NB');</script>
+    <!-- End Google Tag Manager -->
+
 	<meta charset='utf-8'>
 	<meta name="google-site-verification" content="FLSv7rSw3rDfzbknRy83i2PFyff9AQpn9nZksKMqFSA" />
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1">
@@ -66,14 +74,14 @@
 	<script type="text/javascript" src="js/velocity.min.js"></script>
 	<script type="text/javascript" src="js/app.js"></script>
 	
-	<script>
-		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-		ga('create', 'UA-55036688-1', 'auto');
-		ga('send', 'pageview');
-	</script>
+<!--	<script>-->
+<!--		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){-->
+<!--		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),-->
+<!--		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)-->
+<!--		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');-->
+<!---->
+<!--		ga('create', 'UA-55036688-1', 'auto');-->
+<!--		ga('send', 'pageview');-->
+<!--	</script>-->
 </body>
 </html>
