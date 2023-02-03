4dbfdf0574d1be92156b3790e04a29109d200a88
Matthieu <hello@matthieurebillard.fr>
2023-02-03 01:25:59 +0100

    Initialisation de la page principale

---
+++ README.md
@@ -0,0 +1,1 @@
+## Website matthieurebillard.fr
---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="Page">
+    <div class="Page-bg">
+      <cite>Photo de <a href="https://unsplash.com/@justatony?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank">Tony Litvyak</a></cite>
+    </div>
+    <div class="Page-content">
+      <h1>Matthieu Rébillard</h1>
+      <h2>Lead front-end developer</h2>
+      <a href="https://www.linkedin.com/in/matthieurebillard/" target="_blank" class="Page-cta">
+        <img src="/logo-linkedin.svg" alt="logo LinkedIn" width="50"/>
+      </a>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.Page {
+  position: relative;
+  color: var(--color-white);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-width: 320px;
+  min-height: 100vh;
+  background-color: var(--color-black);
+  //background-color: white;
+
+  &-bg {
+    display: flex;
+    align-items: flex-end;
+    justify-content: flex-end;
+    padding: 1rem;
+    position: absolute;
+    z-index: 1;
+    inset: 0;
+    background-image: url('/bg-desktop-dark-3200-2600.jpg');
+    background-size: cover;
+    opacity: 0.5;
+
+    cite {
+      font-size: 1.6rem;
+    }
+  }
+
+  &-content {
+    position: relative;
+    z-index: 2;
+    text-align: center;
+
+    h1 {
+      font-size: 6.4rem;
+    }
+
+    h2 {
+      font-size: 4rem;
+      margin-top: 2rem;
+    }
+  }
+
+  &-cta {
+    display: block;
+    width: 4rem;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 7rem;
+  }
+}
+</style>
