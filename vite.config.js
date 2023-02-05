669142bf7123a29e3bf9b334f6f76d5ac4665927
Matthieu <hello@matthieurebillard.fr>
2023-02-05 12:35:30 +0100

    Environnement de build

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,24 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-// import fs from 'fs'
+import fs from 'fs'
 import basicSsl from '@vitejs/plugin-basic-ssl'
+console.log(process.env.NODE_ENV);
 
-// https://vitejs.dev/config/
-export default defineConfig({
+const config = {
   plugins: [
     vue(),
-    //basicSsl(),
-  ],
-  // server: {
-  //   https: {
-  //     key: fs.readFileSync('/Users/hyperclub/ca/certificat.key'),
-  //     cert: fs.readFileSync('/Users/hyperclub/ca/certificat.crt')
-  //   }
-  // }
-})
+  ]
+}
+
+if(process.env.NODE_ENV === 'development') {
+  config.server = {
+    https: {
+      key: fs.readFileSync('/Users/hyperclub/ca/certificat.key'),
+      cert: fs.readFileSync('/Users/hyperclub/ca/certificat.crt')
+    }
+  }
+  //config.plugins.push(basicSsl())
+}
+
+// https://vitejs.dev/config/
+export default defineConfig(config)
